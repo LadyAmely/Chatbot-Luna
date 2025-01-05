ed8911bbc66023db1e7fb3d38562bddbe48fbfdd
Amelia <nawrotamelia3@gmail.com>
2025-01-05 02:32:41 +0100

    client

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import "../assets/styles/components/chat/Chat.css";
 import ChatInput from "../components/chat/ChatInput";
-import ChatHistory from "../components/chat/ChatHistory";
-import DesignHeader from "../components/DesignHeader/DesignHeader";
+
 
 function ChatPage(){
 
@@ -12,11 +11,7 @@ function ChatPage(){
                 <div className="chat-text">Chat with Luna</div>
                 <ChatInput/>
                 <div className="chat-line"></div>
-                <div className="chat-text-above-history">Here’s what we talked about</div>
-                <ChatHistory/>
-                 <DesignHeader/>
             </div>
-
         </div>
     );
 
